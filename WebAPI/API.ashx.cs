541bb604a4a2486ee0d75ad5ffe1ee256fc1e731
WangJun <wangjun19850215@live.cn>
2017-10-25 20:03:49 +0800

    验证ajax服务通畅性

---
+++ WebAPI/API.ashx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI
+{
+    /// <summary>
+    /// API 的摘要说明
+    /// </summary>
+    public class API : IHttpHandler
+    {
+        /// <summary>
+        /// 服务发现
+        /// 服务路由
+        /// 对接缓存
+        /// </summary>
+        /// <param name="context"></param>
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Headers.Add("Access-Control-Allow-Origin", "*"); //设置请求来源不受限制
+            context.Response.Headers.Add("Access-Control-Allow-Headers", "X-Requested-With");
+            context.Response.Headers.Add("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS"); //请求方式
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
