2806c6ae0f071f3b50f6fa757f6bee45d8a87fe3
WangJun <wangjun19850215@live.cn>
2017-12-28 17:51:39 +0800

    修改前提交

--- WebAPI/API.ashx.cs
+++ WebAPI/API.ashx.cs
@@ -24,32 +24,48 @@ namespace WebAPI
             context.Response.Headers.Add("Access-Control-Allow-Origin", "*"); //设置请求来源不受限制
             context.Response.Headers.Add("Access-Control-Allow-Headers", "X-Requested-With");
             context.Response.Headers.Add("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS"); //请求方式
-            context.Response.ContentType = "application/json";
             this.Execute(context);
         }
 
 
         public void Execute(HttpContext context)
         {
-            //var param =Convertor.FromJsonToDict2(context.Request.Form[0]);
-            var classFullName = context.Request.QueryString["c"];
-            var methodName = context.Request.QueryString["m"];
-            //var dllPath = context.Server.MapPath("./bin/WangJun.Stock.dll");
-            //Assembly ass = Assembly.LoadFrom(dllPath);
-            //var obj = ass.CreateInstance("WangJun.Stock.StockAPI");
-            var target = this.GetTargetObject(classFullName, methodName);
-            var param = new object[] { };
-            var method = target.GetType().GetMethod(methodName);
-            var res1 = method.Invoke(target, param);
+            if(0 == context.Request.Form.Count) ///Get方式调用
+            {
+                var classFullName = context.Request.QueryString["c"];
+                var methodName = context.Request.QueryString["m"];
+                var paramString = context.Request.QueryString["p"];// Convertor.DecodeBase64(context.Request.QueryString["p"]);
+                 
+                var target = this.GetTargetObject(classFullName, methodName);
+                var param = (!string.IsNullOrWhiteSpace(paramString)) ? new object[] { paramString } : new object[] { };
+                var method = target.GetType().GetMethod(methodName);
+                var res1 = method.Invoke(target, param);
+                if(res1 is string)
+                {
+                    context.Response.ContentType = "text/html";
+                    context.Response.Write(res1);
+                }
+                else if(res1 is object)
+                {
+                    context.Response.ContentType = "application/json";
+                var json = Convertor.FromObjectToJson(res1);
+                context.Response.Write(json);   
+                }
 
-            var json = Convertor.FromObjectToJson(res1);
-            context.Response.Write(json);
+            }
+            else
+            {
+                //var param =Convertor.FromJsonToDict2(context.Request.Form[0]);
+            }
+
         }
         protected object GetTargetObject(string classFullName,string methodName)
         {
             var dict = new Dictionary<string, string>();
             dict.Add("WangJun.Stock.StockAPI." + methodName, HttpContext.Current.Server.MapPath("./bin/WangJun.Stock.dll"));
+            dict.Add("WangJun.Stock.DataSourceSINA." + methodName, HttpContext.Current.Server.MapPath("./bin/WangJun.Stock.dll"));
             dict.Add("WangJun.NetLoader.So360." + methodName, HttpContext.Current.Server.MapPath("./bin/WangJun.NetLoader.dll"));
+            dict.Add("WangJun.DB.YunConfig." + methodName, HttpContext.Current.Server.MapPath("./bin/WangJun.DB.dll"));
             var dllPath = dict[classFullName+"." + methodName];
             Assembly ass = Assembly.LoadFrom(dllPath);
             var obj = ass.CreateInstance(classFullName);
