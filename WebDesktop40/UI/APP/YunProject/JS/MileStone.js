f9d87a2f4cf6efcb3f58325acfba97bb37758803
WangJun <wangjun19850215@live.cn>
2018-02-26 03:39:59 +0800

    添加项目管理里程碑界面

---
+++ WebDesktop40/UI/APP/YunProject/JS/MileStone.js
@@ -0,0 +1,15 @@
+﻿var Milestone = {
+}
+
+
+
+Milestone.AddCheckPoint = function (target) {
+    var itemHtml = $("#tpl_Milestone_1").html();
+    $(itemHtml).insertBefore($(event.target));
+}
+
+Milestone.AddTask = function () {
+    var itemHtml = $("#tpl_Milestone_2").html();
+    $(itemHtml).insertBefore($(event.target));
+
+}
