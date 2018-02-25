bbd659e5e455b2c1bb04752fcae8b3d30eb5bb49
WangJun <wangjun19850215@live.cn>
2018-02-25 15:48:25 +0800

    打通Login

---
+++ WebDesktop40/JS/SESSION.js
@@ -0,0 +1,22 @@
+﻿var SESSION = {};
+SESSION.Login = function () {
+    var url = "http://localhost:9990/API.ashx?c=WangJun.HumanResource.StaffWebAPI&m=Login";
+    ///保存一个目录
+         var item = {};
+
+        var $ctrls = $("[data-FormName='Default']").each(function () {
+            var propertyName = $(this).attr("data-propertyName");
+            var propertyValue = $(this).attr("data-propertyValue");
+            if (PARAM_CHECKER.IsNotEmptyString(propertyName)) {
+                item[propertyName.trim()] = propertyValue;
+            }
+        });
+
+        var param = [Convertor.ToBase64String(JSON.stringify(item), true), { 0: "base64" }];
+
+        var callback = function (res) {
+            LOGGER.Log(res);
+            window.location.href = "../Desktop/Desktop.html";
+        }
+        NET.PostData(url, callback, param);
+ }
