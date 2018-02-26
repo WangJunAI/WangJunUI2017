2ecf6e857866b33c5f366def1a7019d643b9cffe
WangJun <wangjun19850215@live.cn>
2018-02-26 22:39:47 +0800

    修改里程碑的东西

--- WebDesktop40/UI/APP/YunProject/JS/MileStone.js
+++ WebDesktop40/UI/APP/YunProject/JS/MileStone.js
@@ -14,7 +14,7 @@ Milestone.AddCheckPoint = function (target, data) {
         }
         else {
 
-            var itemHtml = $("#tpl_Milestone_6").html();
+            var itemHtml = $("#tpl_Milestone_6").html().replace("[CheckPointTitle]", data.Title).replace("[CheckPointSummary]", data.Summary);
             $(target).append(itemHtml);
         }
     }
@@ -28,13 +28,13 @@ Milestone.AddTask = function (target, data) {
     }
     else {
         if ("已完成" === data.Status) {
-             itemHtml = $("#tpl_Milestone_3").html();
+            itemHtml = $("#tpl_Milestone_3").html().replace("[Content]", data.Content).replace("[ExpectedEndTime]", data.ExpectedEndTime);
         }
         else if ("未开始" === data.Status) {
-             itemHtml = $("#tpl_Milestone_5").html();
+            itemHtml = $("#tpl_Milestone_5").html().replace("[Content]", data.Content).replace("[ExpectedEndTime]", data.ExpectedEndTime);
         }
         else if ("处理中" === data.Status) {
-             itemHtml = $("#tpl_Milestone_4").html();
+            itemHtml = $("#tpl_Milestone_4").html().replace("[Content]", data.Content).replace("[ExpectedEndTime]", data.ExpectedEndTime);
         }
         else if ("新增按钮" === data.Status) {
             itemHtml = "<li><a href='javascript:;' class='addbtn' onclick= 'Milestone.AddTask()'> 添加新任务</a></li>";
@@ -46,7 +46,16 @@ Milestone.AddTask = function (target, data) {
 
 
 Milestone.LoadData = function () {
-    var data = [{ Status: "", TaskArray: [{ "Status": "已完成" }, { "Status": "未开始" }, { "Status": "处理中" }, { "Status": "新增按钮" }] }, { TaskArray: [{ "Status": "已完成" }, { "Status": "未开始" }, { "Status": "处理中" }, { "Status": "新增按钮" }] }, { Status: "新增按钮", TaskArray:[]}];
+    var data = [{
+        Title: "完成界面代码", Summary: "预计2017年12月18日结束，当前完成度63.5%，剩余时间 4天", Status: ""
+        , TaskArray: [{ "Status": "已完成", "Content": "将数据提交上去", "ExpectedEndTime": "2017/12/12" }, { "Status": "未开始", "Content": "将数据获取出来", "ExpectedEndTime": "2017/12/12" }, { "Status": "处理中", "Content": "最后完整检查一遍", "ExpectedEndTime": "2017/12/12" }, { "Status": "新增按钮" }]
+    }
+        , {
+            Title: "完成JS测试", Summary: "预计2017年12月18日结束，当前完成度23.5%，剩余时间 4天"
+            , TaskArray: [{ "Status": "已完成", "Content": "将数据提交上去", "ExpectedEndTime": "2017/12/12" }, { "Status": "未开始", "Content": "将数据获取出来", "ExpectedEndTime": "2017/12/12" }, { "Status": "处理中", "Content": "最后完整检查一遍", "ExpectedEndTime": "2017/12/12" }, { "Status": "新增按钮" }]
+
+        }
+        , { Status: "新增按钮", TaskArray: [] }];
     Milestone.ShowData(data);
 }
 
