59035998d7b845c8ead8a744f66dd13ea386e496
WangJun <wangjun19850215@live.cn>
2018-03-04 11:33:35 +0800

    文件删除干净,运行成功

---
+++ JS提升后备份/WebDesktop40/UI/APP/YunNote/JS/AppInfo.QuickBar.js
@@ -0,0 +1,14 @@
+﻿//var App = {};
+App.QuickBar = {};
+
+App.QuickBar.Info = {};
+App.QuickBar.Info.ID = "QuickBar";
+App.QuickBar.Info.Name = "汪俊QuickBar";
+
+App.QuickBar.Server = {
+    Url1: "http://localhost:9990/API.ashx?c=WangJun.Doc.DocManager&m=Find&p=0",
+};
+
+
+
+ 
