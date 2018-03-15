119433dc8096a4a196f3d788fcd4a697b803aa88
WangJun <wangjun19850215@live.cn>
2018-03-15 20:21:24 +0800

    完善数据获取

--- WebDesktop40/UI/APP/Staff/JS/Doc.Detail.js
+++ WebDesktop40/UI/APP/Staff/JS/Doc.Detail.js
@@ -26,7 +26,7 @@ Doc.LoadDetail = function () {
     }
 }
 
-Doc.ShowDetail = function (data) {
+Doc.ShowDetail = function (data, option)) {
     if (true === PARAM_CHECKER.IsObject(data)) {
         if (true === PARAM_CHECKER.IsNotEmptyString(data.Content) && "<" === data.Content[0]) {
             UE.getEditor('editor').setContent(data.Content);
--- WebDesktop40/UI/APP/YunProject/JS/Doc.Detail.js
+++ WebDesktop40/UI/APP/YunProject/JS/Doc.Detail.js
@@ -27,7 +27,7 @@
     });
 }
 
-Doc.ShowDetail = function (data) {
+Doc.ShowDetail = function (data,option) {
     if (true === PARAM_CHECKER.IsObject(data)) {
         if (true === PARAM_CHECKER.IsNotEmptyString(data.Content) && "<" === data.Content[0]) {
             UE.getEditor('editor').setContent(data.Content);
