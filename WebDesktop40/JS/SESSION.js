ee5c9a58bf25b1fba0dcc9f924d24f627c8b532e
WangJun <wangjun19850215@live.cn>
2018-07-01 23:53:04 +0800

    完善移动端

--- WebDesktop40/JS/SESSION.js
+++ WebDesktop40/JS/SESSION.js
@@ -1,42 +1,58 @@
-﻿ 
+﻿
 
- 
+
 var SESSION = {
     ServerHost: YunConfig.ServerHost(this) //("localhost" === window.location.hostname) ? window.location.protocol + "//" + window.location.hostname + ":9990" : "http://aifuwu.wang",
 };
 SESSION.LoginUrl = SESSION.ServerHost + "/API.ashx?c=WangJun.App.YunUserAPI&m=Login";
 SESSION.RegisterUrl = SESSION.ServerHost + "/API.ashx?c=WangJun.Admin.AdminWebAPI&m=CreateCompany";
 SESSION.Heartbeat = SESSION.ServerHost + "/API.ashx?c=WangJun.App.YunUserAPI&m=Heartbeat";
-
+SESSION.IsFromPC = (function () {
+    return "WeUI" != $("#PageName").val();
+});
 
 
 SESSION.Login = function () {
+    if (true === SESSION.IsFromPC) {
+        $(event.target).attr("disabled", "disabled");
+        $(event.target).val("登录中,请稍后...");
+    }
+    else {
+
+    }
 
 
-    $(event.target).attr("disabled", "disabled");
-    $(event.target).val("登录中,请稍后...");
     $.cookie("SESSION", JSON.stringify({ "UserID": "Login" }), { path: '/' });
     var url = SESSION.LoginUrl;
     ///保存一个目录
-         var item = {};
+    var item = {};
 
-        var $ctrls = $("[data-FormName='Default']").each(function () {
-            var propertyName = $(this).attr("data-propertyName");
-            var propertyValue = $(this).attr("data-propertyValue");
-            if (PARAM_CHECKER.IsNotEmptyString(propertyName)) {
-                item[propertyName.trim()] = propertyValue;
-            }
-        });
+    var $ctrls = $("[data-FormName='Default']").each(function () {
+        var propertyName = $(this).attr("data-propertyName");
+        var propertyValue = $(this).attr("data-propertyValue");
+        if (PARAM_CHECKER.IsNotEmptyString(propertyName)) {
+            item[propertyName.trim()] = propertyValue;
+        }
+    });
 
-        var param = [Convertor.ToBase64String(JSON.stringify(item), true), { 0: "base64" }];
+    var param = [Convertor.ToBase64String(JSON.stringify(item), true), { 0: "base64" }];
 
-        var callback = function (res) {
-            LOGGER.Log(res);
-            $.cookie("SESSION", JSON.stringify(res), { path: '/' });
-            alert("登陆成功" + res.UserName);
-            window.location.href = "../Desktop/Desktop.html";
-         }
-        NET.PostData(url, callback, param);
+    var callback = null;
+    var callbackPC = function (res) {
+
+        LOGGER.Log(res);
+        $.cookie("SESSION", JSON.stringify(res), { path: '/' });
+        alert("登陆成功" + res.UserName);
+        window.location.href = "../Desktop/Desktop.html";
+    }
+
+    var callbackMobile = function (res) {
+        App.Initial();
+    }
+    callback = (true === SESSION.IsFromPC()) ? callbackPC : callbackMobile;
+
+     
+    NET.PostData(url, callback, param);
 }
 
 SESSION.Current = function () {
@@ -108,17 +124,22 @@ SESSION.Initial = function () {
             $("[data-propertyName='OwnerID']").attr("data-PropertyValue", SESSION.Current().CompanyID);
         }
 
-        
+
     });
 }
 
 SESSION.Register = function () {
-    $(event.target).attr("disabled", "disabled");
-    $(event.target).val("正在注册,请稍后...");
+    if (true === SESSION.IsFromPC) {
+        $(event.target).attr("disabled", "disabled");
+        $(event.target).val("正在注册,请稍后...");
+    }
+    else {
+
+    }
 
 
-    $.cookie("SESSION", JSON.stringify({ "UserID":"CreateCompany"}), { path: '/' });
-    var url = SESSION.RegisterUrl ;
+    $.cookie("SESSION", JSON.stringify({ "UserID": "CreateCompany" }), { path: '/' });
+    var url = SESSION.RegisterUrl;
 
     var item = {};
 
@@ -131,8 +152,8 @@ SESSION.Register = function () {
     });
 
     var param = [Convertor.ToBase64String(JSON.stringify(item), true), { 0: "base64" }];
-
-    var callback = function (res) {
+    var callback = null;
+    var callbackPC = function (res) {
         LOGGER.Log(res);
         if (true === PARAM_CHECKER.IsNotEmptyString()) {
             res = JSON.parse(res);
@@ -141,16 +162,22 @@ SESSION.Register = function () {
         alert("注册成功" + res.CompanyName + " ，将以超级管理员 " + res.UserName + "身份登录");
         window.location.href = "../Desktop/Desktop.html";
     }
+
+    var callbackMobile = function (res) {
+
+    }
+    callback = (true === SESSION.IsFromPC()) ? callbackPC : callbackMobile;
+
     NET.PostData(url, callback, param);
     $.cookie("SESSION", JSON.stringify({}), { path: '/' });
 
     ///动画显示
-    var info = ["正在激活公司服务...", "正在初始化人员和组织管理服务...", "正在初始化新闻服务...", "正在初始化知识库服务...", "正在初始化云笔记服务...", "正在初始化群组服务...", "正在初始化云项目服务...", "正在初始化云盘服务...","正在整合信息,即将完成..."];
+    var info = ["正在激活公司服务...", "正在初始化人员和组织管理服务...", "正在初始化新闻服务...", "正在初始化知识库服务...", "正在初始化云笔记服务...", "正在初始化群组服务...", "正在初始化云项目服务...", "正在初始化云盘服务...", "正在整合信息,即将完成..."];
     setInterval(function () {
         if (0 < info.length) {
             $("#regBtn").val(info.shift());
-        } 
-    }, 2000); 
+        }
+    }, 2000);
 
 }
 
---
+++ WebDesktop40/UI/APP/TouTiaoPhone/weui-master/dist/example/App.js
@@ -0,0 +1,102 @@
+﻿var App = {
+    
+}
+
+App.Initial = function () {
+    App.TabClick();
+    App.LoadTab1();
+    App.LoadTab2();
+    App.LoadTab3();
+}
+
+App.TabClick = function () {
+    $('.weui-tabbar__item').on('click', function () {
+        $(this).addClass('weui-bar__item_on').siblings('.weui-bar__item_on').removeClass('weui-bar__item_on');
+        var targetId = $(this).attr("targetId");
+        $(".weui-tab__panel").hide();
+        $("#" + targetId).show();
+    });
+}
+
+App.AppClick = function () {
+    $("#tab3 .weui-cell").on("click", function () {
+
+        var targetId = $(this).attr("targetId");
+        $(".weui-tab__panel").hide();
+        $("#" + targetId).show();
+    });
+}
+
+App.LoadTouTiao = function () {
+
+}
+
+App.LoadTab1 = function () {
+    $(".weui-tab__panel").hide();
+    $("#" + "tab1").show();
+}
+
+App.LoadTab2 = function () {
+    ///通讯录
+    var $searchBar = $('#searchBar'),
+        $searchResult = $('#searchResult'),
+        $searchText = $('#searchText'),
+        $searchInput = $('#searchInput'),
+        $searchClear = $('#searchClear'),
+        $searchCancel = $('#searchCancel');
+
+    function hideSearchResult() {
+        $searchResult.hide();
+        $searchInput.val('');
+    }
+    function cancelSearch() {
+        hideSearchResult();
+        $searchBar.removeClass('weui-search-bar_focusing');
+        $searchText.show();
+    }
+
+    $searchText.on('click', function () {
+        $searchBar.addClass('weui-search-bar_focusing');
+        $searchInput.focus();
+    });
+    $searchInput
+        .on('blur', function () {
+            if (!this.value.length) cancelSearch();
+        })
+        .on('input', function () {
+            if (this.value.length) {
+                $searchResult.show();
+            } else {
+                $searchResult.hide();
+            }
+        })
+        ;
+    $searchClear.on('click', function () {
+        hideSearchResult();
+        $searchInput.focus();
+    });
+    $searchCancel.on('click', function () {
+        cancelSearch();
+        $searchInput.blur();
+    });
+}
+
+App.LoadTab3 = function () { 
+    $("#tab3 .weui-cell").on("click", function () {
+
+        var targetId = $(this).attr("targetId");
+        $(".weui-tab__panel").hide();
+        $("#" + targetId).show();
+    });
+
+    App.LoadTab5();
+}
+
+App.LoadTab5 = function () {
+    $(".weui-media-box").on("click", function () {
+        var targetId = $(this).attr("targetId");
+        $(".weui-tab__panel").hide();
+        $("#" + targetId).show();
+    });
+
+}
