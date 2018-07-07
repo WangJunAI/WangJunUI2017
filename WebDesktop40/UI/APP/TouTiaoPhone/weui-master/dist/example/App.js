aa8adf8a0381842952cc1a7711f8c8f6df378819
WangJun <wangjun19850215@live.cn>
2018-07-07 10:46:00 +0800

    完善WEUI

--- WebDesktop40/UI/APP/TouTiaoPhone/weui-master/dist/example/App.js
+++ WebDesktop40/UI/APP/TouTiaoPhone/weui-master/dist/example/App.js
@@ -1,6 +1,24 @@
 ﻿var App = {
-    
+    Server: {
+        YunDoc: {
+            Url1: YunConfig.ServerHost(this) + "/API.ashx?c=WangJun.YunDoc.YunDocWebAPI&m=LoadEntityList"
+        }
+    },
+    Pager: { Size: 10 }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 App.Initial = function () {
     App.TabClick();
@@ -87,16 +105,50 @@ App.LoadTab3 = function () {
         var targetId = $(this).attr("targetId");
         $(".weui-tab__panel").hide();
         $("#" + targetId).show();
+        App.ShowMessage();
+        App["Load" + targetId]();
     });
-
-    App.LoadTab5();
+     
 }
 
 App.LoadTab5 = function () {
     TouTiao.Initial("list");
+}
 
 
+App.LoadTab6 = function () {
+    var callback = function (res) {
+        LOGGER.Log(res);
+        App.ShowMessage();
+        var vue = new Vue({
+            el: "#hiddenTab6-1",
+            data: {
+                list: res
+            },
+            mounted: function () {
+                $("#yunDocList").append($("#hiddenTab6-1").children());
+                $("#hiddenTab6-1").empty();
+            },
+            methods: {
+                TimeFormat: function (listItem) {
+                    if (true === PARAM_CHECKER.IsNotEmptyString(listItem.CreateTime) && '/' === listItem.CreateTime[0]) {
+                        listItem.CreateTime = eval("new " + listItem.CreateTime.replace(/\//g, ''));
+                    }
+                    else if (true === PARAM_CHECKER.IsDate(listItem.CreateTime)) {
+                        listItem.CreateTime = Convertor.DateFormat(listItem.CreateTime, "yyyy/MM/dd hh:mm");
+                    }
 
+                    return listItem.CreateTime.toString();
+                }
+            }
+        });
+    }
+    var query =     JSON.stringify({});
+    var protection = JSON.stringify ({});
+    var sort = JSON.stringify ({});
+    var pageIndex = 0;
+    var param = [query, protection, sort, pageIndex, App.Server.Size];
+    NET.PostData(App.Server.YunDoc.Url1, callback, param);
 }
 
 App.Tab11Click = function () {
@@ -104,17 +156,19 @@ App.Tab11Click = function () {
         var targetId = $(this).attr("targetId");
         $(".weui-tab__panel").hide();
         $("#" + targetId).show();
-        TouTiao.LoadArticle();
+        
         
     });
 }
 
-App.ShowMessage = function () {
-    var $toast = $('#toast');
+App.ShowMessage = function (type,message) {
+    var $toast = ("loading" === type) ? $("#loadingToast") : $('#toast');
+    $toast.text(message);
     if ($toast.css('display') != 'none') return;
 
         $toast.fadeIn(100);
         setTimeout(function () {
             $toast.fadeOut(100);
+            $toast.text("未定义任何消息");
         }, 2000); 
  }
